ef271e8fb7bc69a4df80a04db25dad4359c3e583
Michael Audric <mikeaudric123@gmail.com>
2025-12-21 19:56:47 +0700

    nambahin MosaicSolverGA

--- MosaicSolverGA.java
+++ MosaicSolverGA.java
@@ -1,12 +1,12 @@
 public class MosaicSolverGA {
     public static void main(String[] args) {
         // inisialisasi peta
-        
+
 
         // bagian GA
         // 1. inisialisasi populasi
         // 2. seleksi parent untuk populasi berikutnya
-        // 3. crossover
+        // 3. crossover & mutasi
         // 4. ulang dari step 2 sampe fitness = 1 (ketemu solusi)
     }
     
