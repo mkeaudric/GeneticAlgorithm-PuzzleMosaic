3ccc034f6d153c1d36134a76aeec7c347aaf464a
YohanesEthanSetiawan <6182301073@student.unpar.ac.id>
2025-12-22 22:57:56 +0700

    tambahin crossover

--- Crossover.java
+++ Crossover.java
@@ -1,3 +1,23 @@
 public class Crossover {
-    
+    private double crossoverRate;
+
+    public Crossover(double crossoverRate) {
+        this.crossoverRate = crossoverRate;
+    }
+
+    public Individu crossover(Individu parent1, Individu parent2, String crossoverMethod) {
+        Kromosom kromosom1 = parent1.getKromosom().copy();
+
+        switch (crossoverMethod) {
+            case "single-point":
+                    return singlePointCrossover(parent1, parent2);
+            case "uniform":
+                    return uniformCrossover(parent1, parent2);
+            case "two-point":
+                    return twoPointCrossover(parent1, parent2);
+            default:
+                System.err.println("method crossover tidak dikenali: " + crossoverMethod + ". Menggunakan single-point crossover sebagai default.");
+                return singlePointCrossover(parent1, parent2);
+        }
+    }
 }
