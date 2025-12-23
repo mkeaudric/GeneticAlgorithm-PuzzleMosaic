c55f1d7146d3a2f0cb7a793286c37169f87cec7a
Christopher Stevenson <christjason28@gmail.com>
2025-12-23 23:22:32 +0700

    tambah mutasi & two point crossover

--- Selection.java
+++ Selection.java
@@ -9,25 +9,25 @@ public class Selection {
     // rank selection
 
     // tournament selection
-    public Individu tournamentSelection(Populasi populasi, int tournamentSize) {
-        List<Individu> tournamentParticipants = new ArrayList<>();
+    public Individu tournamentSelection(Populasi populasi, int size) {
+        List<Individu> participants = new ArrayList<>();
         List<Individu> allIndividu = populasi.getIndividuList();
 
-        // Pilih peserta turnamen secara acak dari populasi
-        for (int i = 0; i < tournamentSize; i++) {
+        // pilih peserta random dari populasi
+        for (int i = 0; i < size; i++) {
             int randomIndex = RNG.rand.nextInt(allIndividu.size());
-            tournamentParticipants.add(allIndividu.get(randomIndex));
+            participants.add(allIndividu.get(randomIndex));
         }
 
-        // Cari yang terbaik di antara peserta turnamen
-        Individu best = tournamentParticipants.get(0);
-        for (int i = 1; i < tournamentParticipants.size(); i++) {
-            if (tournamentParticipants.get(i).getFitness() > best.getFitness()) {
-                best = tournamentParticipants.get(i);
+        // cari yg terbaik di antara peserta turnamen
+        Individu best = participants.get(0);
+        for (int i = 1; i < participants.size(); i++) {
+            if (participants.get(i).getFitness() > best.getFitness()) {
+                best = participants.get(i);
             }
         }
 
-        // Kembalikan salinan untuk diolah (crossover/mutasi)
+        // return copy utk crossover/mutasi
         return best.copyForOffspring();
     }
 
