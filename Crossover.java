390648a9213705da32d15b2bf2ab26185d5957db
Michael Audric <mikeaudric123@gmail.com>
2025-12-23 22:11:28 +0700

    Implementasi crossover one point, crossover uniform, baru itu aja

--- Crossover.java
+++ Crossover.java
@@ -1,13 +1,13 @@
+// http://ijcs.net/ijcs/index.php/ijcs/article/view/4596/934
 public class Crossover {
-    private double crossoverRate;
+    private FitnessFunction fitnessFunction;
 
-    public Crossover(double crossoverRate) {
-        this.crossoverRate = crossoverRate;
+    public Crossover(FitnessFunction fitnessFunction) {
+        this.fitnessFunction = fitnessFunction;
     }
 
-    public Individu crossover(Individu parent1, Individu parent2, String crossoverMethod) {
-        Kromosom kromosom1 = parent1.getKromosom().copy();
-
+    // kita pake 2 parent, hasilnya 2 anak
+    public Individu[] crossover(Individu parent1, Individu parent2, String crossoverMethod) {
         switch (crossoverMethod) {
             case "single-point":
                     return singlePointCrossover(parent1, parent2);
@@ -19,5 +19,52 @@ public class Crossover {
                 System.err.println("method crossover tidak dikenali: " + crossoverMethod + ". Menggunakan single-point crossover sebagai default.");
                 return singlePointCrossover(parent1, parent2);
         }
+    }
+    
+    private Individu[] singlePointCrossover(Individu parent1, Individu parent2) {
+        int i, length = parent1.getKromosom().getlength();
+        int crossoverPoint = RNG.rand.nextInt(length-1); // sumber : https://www.baeldung.com/java-generating-random-numbers-in-range
+        
+        Kromosom firstChild = parent1.getKromosom().copy();
+        Kromosom secondChild = parent2.getKromosom().copy();
+
+        for(i=crossoverPoint; i < length; i++){
+            // ini karena kita udah setFixedAllele untuk satu populasi, kita gaperlu cek sebenernya karena pasti sama warnanya untuk yang fixed
+            // tapi kita pake aja biar hemat resource komputasi (kalo udah pasti sama gaperlu nuker)
+            // karena pasti sama yang fixed, saya cuma cek 1 anak aja
+            if(!firstChild.getFixedAllele(i)){
+                // tuker allele
+                boolean temp = firstChild.getBit(i);
+                firstChild.setBit(i, secondChild.getBit(i));
+                secondChild.setBit(i, temp);
+            }
+        }
+
+        return new Individu[] {new Individu(firstChild, fitnessFunction), new Individu(secondChild, fitnessFunction)};
+    }
+
+    private Individu[] uniformCrossover(Individu parent1, Individu parent2) {
+        int i, length = parent1.getKromosom().getlength();
+
+        Kromosom firstChild = parent1.getKromosom().copy();
+        Kromosom secondChild = parent2.getKromosom().copy();
+
+        for(i=0; i < length; i++){
+            if(!firstChild.getFixedAllele(i)){
+                // ada probabilitas 50% ketuker, ato engga ketuker
+                if(RNG.rand.nextDouble() < 0.5){
+                    // tuker allele
+                    boolean temp = firstChild.getBit(i);
+                    firstChild.setBit(i, secondChild.getBit(i));
+                    secondChild.setBit(i, temp);
+                }
+            }
+        }
+
+        return new Individu[] {new Individu(firstChild, fitnessFunction), new Individu(secondChild, fitnessFunction)};
+    }
+
+    private Individu[] twoPointCrossover(Individu parent1, Individu parent2) {
+        
     }
 }
