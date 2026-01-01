8bdd681a1f6c1ee67d519c458b36c263677ce898
Michael Audric <mikeaudric123@gmail.com>
2026-01-01 22:05:44 +0700

    nambahin heuristik 7

--- NgecekHeuristik.java
+++ NgecekHeuristik.java
@@ -1,9 +1,9 @@
 public class NgecekHeuristik {
     public static void main(String[] args) throws Exception {
-        String filePeta = "buatNgecekHeuristik3.txt";
+        String filePeta = "buatNgecekHeuristik2.txt";
         MosaicPuzzle puzzle = InputReader.bacaPuzzle(filePeta);
 
-        Kromosom kromosom = new Kromosom(5, puzzle);
+        Kromosom kromosom = new Kromosom(20, puzzle);
         PenandaHeuristik.setFixedAllele(kromosom);
 
         kromosom.printCurrentFixedKromosomAsGrid();
