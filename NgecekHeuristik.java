aaebe0cebe1c888a65d1f1ccce89a66ec1a54ece
Michael Audric <mikeaudric123@gmail.com>
2026-01-02 12:19:40 +0700

    testing heuristik 10

--- NgecekHeuristik.java
+++ NgecekHeuristik.java
@@ -14,14 +14,23 @@ public class NgecekHeuristik {
         // kromosom.setBit(3, 1, false); 
         // kromosom.setFixedAllele(3, 1, true);
 
-        // kromosom.setBit(0, 1, false);
-        // kromosom.setBit(0, 2, true);
-        // kromosom.setBit(0, 3, true);
+        // kromosom.setBit(0, 1, true);
+        // kromosom.setBit(0, 2, false);
+        // kromosom.setBit(0, 3, false);
         // kromosom.setBit(3, 3, false);
+        // kromosom.setBit(3, 1, false);
         // kromosom.setFixedAllele(0, 1, true);
         // kromosom.setFixedAllele(0, 2, true);
         // kromosom.setFixedAllele(0, 3, true);
         // kromosom.setFixedAllele(3, 3, true);
+        // kromosom.setFixedAllele(3, 1, true);
+
+        // kromosom.setBit(1, 1, false);
+        // kromosom.setBit(2, 1, false);
+        // kromosom.setBit(3, 1, false);
+        // kromosom.setFixedAllele(1, 1, true);
+        // kromosom.setFixedAllele(2, 1, true);
+        // kromosom.setFixedAllele(3, 1, true);
 
         PenandaHeuristik.setFixedAllele(kromosom);
 
